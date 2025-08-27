fe70ce589e7347bcd8c46286bda9af64a05cd21b
Rob Lovegrove <rob@Robs-MBP-2.powerhub>
2025-08-27 14:09:43 +0100

    test(DateTime): added tests for toString()

--- tests/unit/test_dateTime.cpp
+++ tests/unit/test_dateTime.cpp
@@ -14,3 +14,26 @@ TEST_CASE("DateTime constructors", "[datetime]") {
         REQUIRE(dt2.getTimestamp() == knownTimestamp);
     }
 }
+
+TEST_CASE("DateTime string formatting", "[datetime]") {
+    SECTION("toString produces readable format") {
+        DateTime dt(1234567890);  // 2009-02-13 23:31:30 UTC
+        std::string str = dt.toString();
+        
+        // Should contain the date components
+        REQUIRE(str.find("2009") != std::string::npos);
+        REQUIRE(str.find("02") != std::string::npos);
+        REQUIRE(str.find("13") != std::string::npos);
+        REQUIRE(str.find("23") != std::string::npos);
+        REQUIRE(str.find("31") != std::string::npos);
+        REQUIRE(str.find("30") != std::string::npos);
+    }
+    
+    // SECTION("toISO860String produces ISO format") {
+    //     DateTime dt(1234567890);  // 2009-02-13 23:31:30 UTC
+    //     std::string iso = dt.toISO860String();
+        
+    //     // Should be ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ
+    //     REQUIRE(iso.find("2009-02-13T23:31:30Z") != std::string::npos);
+    // }
+}
