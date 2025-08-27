418e50d5afbc3eb1f7c1dfe0b6827e6825229d58
Rob Lovegrove <rob@Robs-MBP-2.powerhub>
2025-08-27 18:32:49 +0100

    feat(AuditTrail): Setting up AuditTrail class, this will do all the processing for the Reader.cpp

---
+++ src/AuditTrail/auditTrail.cpp
@@ -0,0 +1,5 @@
+#include "auditTrail.hpp"
+
+AuditTrail::AuditTrail(UUID initialModuleID, std::istream& auditTrailFile, XRefTable& xrefTable) {
+
+}
--- src/AuditTrail/auditTrail.hpp
+++ src/AuditTrail/auditTrail.hpp
@@ -1,3 +1,11 @@
+#include <vector>
+#include <string>
+#include <fstream>
+
+#include "../Utility/uuid.hpp"
+#include "../Utility/dateTime.hpp"
+#include "../Xref/xref.hpp"
+
 struct ModuleTrail {
     uint64_t moduleOffset;
     bool isCurrent;
@@ -5,6 +13,20 @@ struct ModuleTrail {
     DateTime modifiedAt;
     std::string createdBy;
     std::string modifiedBy;
-}
+};
 
-std::vector<ModuleTrail> getAuditTrail(const UUID& moduleId);
+class AuditTrail {
+    private:
+    UUID initialModuleID;
+    std::vector<ModuleTrail> auditTrail;
+
+    public:
+        AuditTrail(UUID initialModuleID, std::istream& auditTrailFile, XRefTable& xrefTable);
+
+        std::vector<ModuleTrail> getModuleTrail() const {
+            return auditTrail;
+        }
+
+        UUID getInitialModuleID() const { return initialModuleID; }
+
+};
