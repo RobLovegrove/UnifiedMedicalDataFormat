ce2793cead0fe024253618256cec8f5aa77c050a
Rob Lovegrove <rob@Robs-MBP-2.powerhub>
2025-08-27 14:10:17 +0100

    feat(DateTime): Implemented toString() method

--- src/Utility/dateTime.cpp
+++ src/Utility/dateTime.cpp
@@ -22,7 +22,12 @@ DateTime DateTime::readBinary(std::istream& in) {
 
 // Helper Methods
 std::string DateTime::toString() const {
-
+    auto time_t = std::chrono::system_clock::to_time_t(timePoint);
+    std::tm* timeinfo = std::gmtime(&time_t);
+    
+    std::ostringstream oss;
+    oss << std::put_time(timeinfo, "%Y-%m-%d %H:%M:%S UTC");
+    return oss.str();
 }
 
 std::string DateTime::toISO860String() const {
