8a9dc97ade167efa0712e173b1baa569c89317d6
Rob Lovegrove <rob@Robs-MBP-2.powerhub>
2025-08-27 14:11:09 +0100

    test(dateTime): Added test for toISO860String

--- tests/unit/test_dateTime.cpp
+++ tests/unit/test_dateTime.cpp
@@ -29,11 +29,11 @@ TEST_CASE("DateTime string formatting", "[datetime]") {
         REQUIRE(str.find("30") != std::string::npos);
     }
     
-    // SECTION("toISO860String produces ISO format") {
-    //     DateTime dt(1234567890);  // 2009-02-13 23:31:30 UTC
-    //     std::string iso = dt.toISO860String();
+    SECTION("toISO860String produces ISO format") {
+        DateTime dt(1234567890);  // 2009-02-13 23:31:30 UTC
+        std::string iso = dt.toISO860String();
         
-    //     // Should be ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ
-    //     REQUIRE(iso.find("2009-02-13T23:31:30Z") != std::string::npos);
-    // }
+        // Should be ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ
+        REQUIRE(iso.find("2009-02-13T23:31:30Z") != std::string::npos);
+    }
 }
